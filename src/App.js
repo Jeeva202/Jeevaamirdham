3eeb0e9e58ee62181e20ef52731e2d112ed75a41
Jeevaganesh.r <Jeevaganesh.R@mu-sigma.com>
2024-10-24 13:40:02 +0530

    favIcon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import TopNavbar from './components/topNavbar/topNavbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopNavbar/>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/"} replace />} />
+          <Route path="/1" element={<Login />} />
+          <Route path="/2" element={<AppScreen />} />
+        </Routes>
+      </BrowserRouter> */}
+    </>
   );
-}
+} 
 
 export default App;
---
+++ src/constants/screenData.ts
@@ -0,0 +1,15 @@
+import phoneIcon from '../assets/icons/phonecall.svg'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import traslateIcon from '../assets/icons/engToTamil.svg'
+
+export const navBanner = {
+    icons:{
+        phone: phoneIcon,
+        facebook: FacebookOutlinedIcon,
+        instagram: InstagramIcon,
+        twitter: TwitterIcon,
+        traslate: traslateIcon
+    }
+}
