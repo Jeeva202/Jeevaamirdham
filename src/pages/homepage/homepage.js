5817c40c07028acabe6ebc92731b868203dd37f0
Ponmadasamy P <Ponmadasamy.P1@mu-sigma.com>
2024-11-09 20:27:21 +0530

    created carousel in the homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -13,7 +13,25 @@ import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRound
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 
 export default function HomePage() {
+    const carouselImages = [
+        "/assets/images/carousel1.svg",
+        "/assets/images/carousel2.svg"
+    ];
+    const [step, setStep] = useState(0);
+    const numSteps = carouselImages.length;
 
+    const handleNextStep = () => {
+        setStep(prevStep => (prevStep + 1) % numSteps);
+    };
+
+    const handlePreviousStep = () => {
+        setStep(prevStep => (prevStep - 1 + numSteps) % numSteps);
+    };
+
+    useEffect(() => {
+        const timer = setTimeout(handleNextStep, 4000);
+        return () => clearTimeout(timer);
+    }, [step]);
     return (
         <Container maxWidth="lg">
 
@@ -21,17 +39,57 @@ export default function HomePage() {
                 <div className="NavBar">
 
                 </div>
+                <div className="carousel">
+                    <Card sx={{
+                        // background: "rgba(255, 255, 255, 0.15)",
+                        // backdropFilter: "blur(7.5px)",
+                        width: "100%",
+                        // maxWidth: "37rem",
+                        borderRadius: "10px",
+                        border: "1px solid #ccc",
+                        boxShadow: "none"
+                    }}>
+                        <CardContent sx={{ padding: "16px", borderRadius: "5px" }}>
+                            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                <IconButton onClick={handlePreviousStep} disabled={step === 0}>
+                                    <ArrowBackIosNewRoundedIcon sx={{ color: '#8C7ED7', fontSize: "1.5rem" }} />
+                                </IconButton>
+                                <Box component="img" src={carouselImages[step]} alt="carousel" sx={{ width:"95%",height: "100%", borderRadius: "10px" }} />
+                                <IconButton onClick={handleNextStep} disabled={step === numSteps - 1}>
+                                    <ArrowForwardIosRoundedIcon sx={{ color: '#8C7ED7', fontSize: "1.5rem" }} />
+                                </IconButton>
+                            </Box>
+                            <Box sx={{ display: 'flex', justifyContent: "center", marginTop: "1rem" }}>
+                                {[...Array(numSteps)].map((_, index) => (
+                                    <Box
+                                        key={index}
+                                        sx={{
+                                            width: 8,
+                                            height: 8,
+                                            borderRadius: '50%',
+                                            backgroundColor: index === step ? '#fff' : '#8C7ED7',
+                                            margin: '0 4px',
+                                            cursor: 'pointer',
+                                        }}
+                                        onClick={() => setStep(index)}
+                                    />
+                                ))}
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </div>
+
                 <div className="PopBooks">
                     <BooksSection />
                 </div>
                 <div className="YouTube">
-                    <PranavamTV/>
+                    <PranavamTV />
                 </div>
                 <div className="News">
                     {/* <LatestNews/> */}
                 </div>
                 <div className="playStore">
-                    <Playstore/>
+                    <Playstore />
                 </div>
 
                 <NewsLetter />
